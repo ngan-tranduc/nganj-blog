98b05bc7d3ea84c4fc21832b63b39dcbf5c25b85
ngan-tranduc <ducngan.work@gmail.com>
2025-08-31 15:36:31 +0700

    2.Migrate database version 1

---
+++ src/NganjBlog.Data/NganjBlogContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace NganjBlog.Data
+{
+    public class NganjBlogContextFactory : IDesignTimeDbContextFactory<NganjBlogContext>
+    {
+        public NganjBlogContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<NganjBlogContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new NganjBlogContext(builder.Options);
+        }
+    }
+}
